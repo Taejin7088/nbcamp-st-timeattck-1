d0e9293ea0bbd8ff8988cfa6a095d04a3b4eb195
taejin7088 <gog70888@gmail.com>
2025-01-27 12:41:57 +0900

    제출

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,98 @@ import Table from "./components/Table.jsx";
 import Form from "./components/Form.jsx";
 
 function App() {
-  const [countryList, setCountryList] = useState([
-    { countryName: "한국", goldCount: 10, silverCount: 10, bronzeCount: 10 },
-    { countryName: "중국", goldCount: 10, silverCount: 10, bronzeCount: 10 },
-  ]);
+  const countryListInLocalstorage = JSON.parse(localStorage.getItem(0));
+  const [countryList, setCountryList] = useState(countryListInLocalstorage);
+
+  const sortByGoldCount = (countryList) => {
+    if (soltOtion === "gold") {
+      countryList.sort((a, b) => {
+        return b.goldCount - a.goldCount;
+      });
+    }
+    if (soltOtion === "total") {
+      countryList.sort((a, b) => {
+        return (
+          b.goldCount +
+          b.silverCount +
+          b.bronzeCount -
+          (a.goldCount + a.silverCount + a.bronzeCount)
+        );
+      });
+    }
+  };
+
+  const isTempCountryinCountryList = ({ countryName }) => {
+    return countryList.some((country) => {
+      return country.countryName === countryName;
+    });
+  };
 
   const addCountry = (tempCountry) => {
-    setCountryList([...countryList, tempCountry]);
+    if (isTempCountryinCountryList(tempCountry)) {
+      alert("이미있는국가");
+      return;
+    }
+    const tempCountryList = [...countryList, tempCountry];
+    sortByGoldCount(tempCountryList);
+    setCountryList(tempCountryList);
+    localStorage.setItem(0, JSON.stringify(tempCountryList));
   };
 
   const removeCountry = (targetCountryName) => {
     const tempCountryList = countryList.filter((country) => {
       return country.countryName !== targetCountryName;
     });
+    sortByGoldCount(tempCountryList);
     setCountryList(tempCountryList);
+    localStorage.setItem(0, JSON.stringify(tempCountryList));
   };
 
   const updateCountry = (tempCountry) => {
+    if (!isTempCountryinCountryList(tempCountry)) {
+      alert("포함안된국가");
+      return;
+    }
     const tempCountryList = countryList.map((country) => {
       if (tempCountry.countryName === country.countryName) {
         return tempCountry;
       }
       return country;
     });
+    sortByGoldCount(tempCountryList);
     setCountryList(tempCountryList);
+    localStorage.setItem(0, JSON.stringify(tempCountryList));
   };
 
+  let soltOtion = "gold";
   return (
     <>
+      <div>
+        <label htmlFor="gold">금메달수</label>
+        <input
+          type="radio"
+          name="sort"
+          id="gold"
+          onChange={() => {
+            soltOtion = "gold";
+            const tempCountryList = [...countryList];
+            sortByGoldCount(tempCountryList);
+            setCountryList(tempCountryList);
+          }}
+        />
+        <label htmlFor="total">총메달</label>
+        <input
+          type="radio"
+          name="sort"
+          id="total"
+          onChange={() => {
+            soltOtion = "total";
+            const tempCountryList = [...countryList];
+            sortByGoldCount(tempCountryList);
+            setCountryList(tempCountryList);
+          }}
+        />
+      </div>
       <div>
         <Form addCountry={addCountry} updateCountry={updateCountry} />
       </div>
